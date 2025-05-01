c7321fb83643b58a02d781f253507b8c21aab9a1
artysubs1 <artysubs1@gmail.com>
2025-05-01 18:50:16 -0300

    Update MGotoCommand.java

--- src/main/java/baritone/command/defaults/MGotoCommand.java
+++ src/main/java/baritone/command/defaults/MGotoCommand.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 /**
  * Custom mgoto command for Baritone 1.21.3.
